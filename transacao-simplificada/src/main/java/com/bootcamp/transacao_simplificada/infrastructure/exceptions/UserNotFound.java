e0ed10543c40c628c4dbed2b39ee1ce34bca0594
Danieldiaxf <danieldias0312@gmail.com>
2026-03-22 10:08:07 -0300

    Feat: Execao Usuario nao encontrado

---
+++ transacao-simplificada/src/main/java/com/bootcamp/transacao_simplificada/infrastructure/exceptions/UserNotFound.java
@@ -0,0 +1,9 @@
+package com.bootcamp.transacao_simplificada.infrastructure.exceptions;
+
+public class UserNotFound extends RuntimeException{
+
+    public UserNotFound(String mensagem){
+        super(mensagem);
+    }
+
+}
