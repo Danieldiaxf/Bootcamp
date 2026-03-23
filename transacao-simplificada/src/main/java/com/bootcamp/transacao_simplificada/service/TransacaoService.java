c19e4572253294ed14d29efc0f7487610ac6355a
Danieldiaxf <danieldias0312@gmail.com>
2026-03-23 20:44:46 -0300

    Feat: Transacao Service

---
+++ transacao-simplificada/src/main/java/com/bootcamp/transacao_simplificada/service/TransacaoService.java
@@ -0,0 +1,96 @@
+package com.bootcamp.transacao_simplificada.service;
+
+import com.bootcamp.transacao_simplificada.infrastructure.controller.TransacaoDTO;
+import com.bootcamp.transacao_simplificada.infrastructure.entity.Carteira;
+import com.bootcamp.transacao_simplificada.infrastructure.entity.TipoUsuario;
+import com.bootcamp.transacao_simplificada.infrastructure.entity.Transacoes;
+import com.bootcamp.transacao_simplificada.infrastructure.entity.Usuario;
+import com.bootcamp.transacao_simplificada.infrastructure.exceptions.BadRequestException;
+import com.bootcamp.transacao_simplificada.infrastructure.repository.TransacaoRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class TransacaoService {
+
+    private final UsuarioService usuarioService;
+    private final AutorizacaoService autorizacaoService;
+    private final CarteiraService carteiraService;
+    private final TransacaoRepository repository;
+    private final NotificacaoService notificacaoService;
+
+    @Transactional
+    public void transferirValores( TransacaoDTO transacaoDTO ) {
+        Usuario pagador = usuarioService.buscarUsuario( transacaoDTO.payer( ) );
+        Usuario recebedor = usuarioService.buscarUsuario(transacaoDTO.payee( ) );
+
+        validaPagadorLojista( pagador );
+        validarSaldoUsuario( pagador, transacaoDTO.value( ) );
+        validarTransferencia();
+
+
+        pagador.getCarteira().setSaldo( pagador.getCarteira().getSaldo( ).subtract( transacaoDTO.value( ) ) );
+        atualiarSaldoCarteira( pagador.getCarteira( ) );
+        recebedor.getCarteira().setSaldo( pagador.getCarteira().getSaldo( ).add( transacaoDTO.value( ) ) );
+        atualiarSaldoCarteira( recebedor.getCarteira( ) );
+
+
+        Transacoes transacoes = Transacoes.builder()
+                .valor( transacaoDTO.value( ) )
+                .pagador( pagador )
+                .recebedor( recebedor )
+                .build();
+
+        repository.save( transacoes );
+        enviarNotificacao();
+    }
+
+    private void validaPagadorLojista( Usuario usuario ) {
+        try{
+            if( usuario.getTipoUsuario( ).equals( TipoUsuario.LOJISTA ) ) {
+                throw new IllegalArgumentException( "Transacao nao autorizada para esse tipo de usuario" );
+            }
+        } catch ( Exception e ) {
+            throw new IllegalArgumentException( e.getMessage( ) );
+        }
+    }
+
+    private void validarSaldoUsuario( Usuario usuario, BigDecimal valor ) {
+       try{
+           if( usuario.getCarteira(  ).getSaldo(  ).compareTo( valor ) < 0 ) {
+                throw new IllegalArgumentException( "Trasacao nao autorizada! Saldo insuficiente!" );
+           }
+       } catch ( Exception e ) {
+           throw new IllegalArgumentException( e.getMessage( ) );
+       }
+    }
+
+    private void validarTransferencia(  ) {
+        try{
+            if( !autorizacaoService.validarTransferencia( ) ) {
+                throw new IllegalArgumentException( "Trasacao nao autorizada pela API!" );
+            }
+        } catch ( Exception e ) {
+            throw new IllegalArgumentException( e.getMessage( ) );
+        }
+    }
+
+    public void atualiarSaldoCarteira(Carteira carteira) {
+        carteiraService.salvar( carteira );
+    }
+
+    private void enviarNotificacao(  ) {
+        try{
+            notificacaoService.enviarNotificacao( );
+
+        } catch ( HttpClientErrorException e) {
+                throw new BadRequestException( "Erro ao enviar notificacao!!!" );
+        }
+    }
+
+}
