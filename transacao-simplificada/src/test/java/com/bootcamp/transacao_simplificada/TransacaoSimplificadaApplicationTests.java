7662b66c0ee54cdcd5d2187544c97e62d7bc96a6
Danieldiaxf <danieldias0312@gmail.com>
2026-05-17 12:14:51 -0300

    git commit -m "fix: add test profile to TransacaoSimplificadaApplicationTests"

--- transacao-simplificada/src/test/java/com/bootcamp/transacao_simplificada/TransacaoSimplificadaApplicationTests.java
+++ transacao-simplificada/src/test/java/com/bootcamp/transacao_simplificada/TransacaoSimplificadaApplicationTests.java
@@ -1,14 +1,15 @@
 package com.bootcamp.transacao_simplificada;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TransacaoSimplificadaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
